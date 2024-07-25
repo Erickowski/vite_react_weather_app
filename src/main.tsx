2619cdd059cbe6e9f28c872f3251d6649bc17658
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-07-24 19:26:05 -0600

    Remove boilerplate files and create Login view

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Login } from "./views/login";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Login />
+  </React.StrictMode>
+);

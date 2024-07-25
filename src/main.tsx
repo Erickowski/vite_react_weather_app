6ef135509046f029ee09c1233aa146e2dc12c391
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-07-24 19:29:32 -0600

    Add configuration for path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Login } from "./views/login";
+import { Login } from "@src/views";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
